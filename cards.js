05f1b0834ead7083872bae53fa373b7d69ccbbd0
kpack <kevinpackler@gmail.com>
2017-08-05 08:55:36 -0400

    change

---
+++ cards.js
@@ -0,0 +1,13 @@
+
+var BasicFlashcard = require('./basic-flashcard.js');
+
+var ClozeFlashcard = require('./cloze-flashcard.js');
+
+var inquirer = require('inquirer');
+
+var fs = require('fs');
+
+//set up inquirer to ask questions
+//display current flash cards or create new
+//create prompts on questions to ask
+
