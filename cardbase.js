a4a7ca2e4abdaed81a04db01bf792ea9ddc8a200
kpack <kevinpackler@gmail.com>
2017-08-06 21:08:43 -0400

    change

---
+++ cardbase.js
@@ -0,0 +1,12 @@
+[
+  {
+    "type": "BasicCard",
+    "front": "Who was the first president of the United States?",
+    "back": "George Washington"
+  },
+  {
+    "type": "ClozeCard",
+    "text": "....was the first president of the United States",
+    "cloze": "George Washington"
+  }
+]
